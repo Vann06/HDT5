60e02ff9f47f66cdc3e1a42e3b26aff2ec512e7f
Vann06 <127684976+Vann06@users.noreply.github.com>
2024-02-29 22:47:25 -0600

    Update Procesos.py

--- Procesos.py
+++ Procesos.py
@@ -3,37 +3,45 @@
 #Vianka Castro
 #23201
 
-#Hoja de Trabajo 5 -
+#Hoja de Trabajo 5
 
 
 import simpy
 import random
 import numpy as np
 
-random_seed = 42
-RAM_cap = 100
-intervalo_de_creacion = 1
-instrucciones_CPU = 6
-num_procesos = 200
+RANDOM_SEED = 42
+RAM_CAP = 200
+INTERVALO_CREACION = 5
+INSTRUCCIONES_CPU_POR_UNIDAD_DE_TIEMPO = 1
 
-tiempos_procesos = []
+
+random.seed(RANDOM_SEED)
 
 def main():
-    random.seed(random_seed)
 
-    env = simpy.Environment()
-    RAM = simpy.Container(env, capacity=RAM_cap, init=RAM_cap)
-    CPU = simpy.Resource(env, capacity=2)
+    tiempos_procesos = []
+    num_procesos = int(input("Ingrese el número de procesos a simular: "))
+    for i in range(num_procesos):
+        env = simpy.Environment()
+        RAM = simpy.Container(env, capacity=RAM_CAP, init=RAM_CAP)
+        CPU = simpy.Resource(env, capacity=1)  # CPU con capacidad 1 (un solo proceso a la vez)
+        env.process(generar_procesos(env, RAM, CPU, tiempos_procesos, num_procesos))
+        env.run(until=10)  # Ejecutar la simulación durante 150 unidades de tiempo
 
-    env.process(generar_procesos(env, RAM, CPU))
+    print(f"para {num_procesos} :")
+    calcular_estadisticas(tiempos_procesos)
 
-    env.run(until=100)  # Ejecutar la simulación durante 100 unidades de tiempo
+def generar_procesos(env, RAM, CPU, tiempos_procesos, num_procesos):
+    num_proceso = 0
+    for num_proceso in range(1, num_procesos + 1):
+        tiempo_inicio = env.now
+        yield env.timeout(random.expovariate(1.0 / INTERVALO_CREACION))
+        env.process(crear_proceso(env, f"Proceso {num_proceso}", RAM, CPU, tiempos_procesos, tiempo_inicio))
 
-    calcular_estadisticas()
 
+def crear_proceso(env, nombre, RAM, CPU, tiempos_procesos, tiempo_inicio):
 
-def crear_proceso(env, nombre, RAM, CPU):
-    inicio = env.now
     memoria_necesaria = random.randint(1, 10)
     instrucciones_totales = random.randint(1, 10)
 
@@ -45,47 +53,41 @@ def crear_proceso(env, nombre, RAM, CPU):
 
     while instrucciones_faltantes > 0:
         with CPU.request() as req:
-            print(f"{nombre} está esperando en {env.now}")
+            print(f"{nombre} Solicitando ejecución del CPU en tiempo  {env.now}")
             yield req
 
             print(f"{nombre} obtiene CPU en {env.now}")
-            instrucciones_CPU = min(instrucciones_faltantes, instrucciones_totales)
-            yield env.timeout(1)  # Simular tiempo de ejecución
-
-            instrucciones_faltantes -= instrucciones_CPU
+            yield env.timeout(1)  # Tiempo de ejecución de 1 unidad de tiempo
+            instrucciones_faltantes -= INSTRUCCIONES_CPU_POR_UNIDAD_DE_TIEMPO
 
             if instrucciones_faltantes <= 0:
                 tiempo_fin = env.now
-                tiempo_proceso = tiempo_fin - inicio
+                tiempo_proceso = tiempo_fin - tiempo_inicio
                 print(f"{nombre} completó el proceso en tiempo {env.now}")
-                tiempos_procesos.append(tiempo_proceso)  # Se agrega el tiempo de proceso a la lista
-                yield RAM.put(memoria_necesaria)  # Liberar memoria
-                break
+                tiempos_procesos.append(tiempo_proceso)
+
 
             print(f"{nombre} procesando en CPU en tiempo {env.now}")
-            espera = random.randint(1, 2)
 
+            espera = random.randint(1,2) # Generar número al azar para verificar si pasa a Waiting
             if espera == 1:
-                tiempo_espera = random.randint(1, 10)
-                print(f"{nombre} esperando por I/O durante {tiempo_espera} unidades de tiempo en t: {env.now}")
-                yield env.timeout(tiempo_espera)
+                print(f"{nombre} pasa a estado Waiting en tiempo {env.now}")
+                yield env.timeout(random.randint(1, 10))
+                print(f"{nombre} regresa a estado Ready en tiempo {env.now}")
             elif espera == 2:
-                print(f"{nombre} listo para ejecutar (ir a ready) nuevamente en t: {env.now}")
+                print(f"{nombre} esta listo {env.now}")
                 continue
 
-def generar_procesos(env, RAM, CPU):
-    num_procesos = 0
-    while True:
-        yield env.timeout(random.expovariate(1.0 / intervalo_de_creacion))
-        num_procesos += 1
-        env.process(crear_proceso(env, f"Proceso {num_procesos}", RAM, CPU))
+            yield RAM.put(memoria_necesaria)  # Liberar memoria
 
-def calcular_estadisticas():
+
+def calcular_estadisticas(tiempos_procesos):
     tiempo_promedio = np.mean(tiempos_procesos)
     desviacion_estandar = np.std(tiempos_procesos)
 
     print(f"Tiempo promedio de procesamiento: {tiempo_promedio}")
     print(f"Desviación estándar: {desviacion_estandar}")
+
 
 if __name__ == '__main__':
     main()
